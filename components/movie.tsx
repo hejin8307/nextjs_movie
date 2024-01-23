f1d4551a03f1878447143fa4c5b394e90753696d
hejin8307 <hejin8307@naver.com>
2024-01-23 22:02:55 +0900

    movie 컴포넌트로 분리

---
+++ components/movie.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+interface IMovieProps {
+  title: string;
+  id: string;
+  poster_path: string;
+}
+
+export default function Movie({ title, id, poster_path }: IMovieProps) {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`movies/${id}`);
+  };
+  return (
+    <div className="grid grid-rows-[1fr, auto] gap-5 cursor-pointer place-items-center">
+      <img
+        className="max-w-full min-h-full rounded-xl transition-opacity duration-3000 ease-in-out opacity-70 hover:opacity-100"
+        src={poster_path}
+        alt={title}
+        onClick={handleClick}
+      />
+      <Link className="text-center" href={`/movies/${id}`}>
+        {title}
+      </Link>
+    </div>
+  );
+}
