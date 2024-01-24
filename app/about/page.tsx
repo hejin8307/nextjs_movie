ad1289e070dabeca53517825074b773830a17d0b
hejin8307 <hejin8307@naver.com>
2024-01-24 10:32:13 +0900

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,22 @@ export const metadata = {
 
 export default function About() {
   return (
-    <>
-      <h1>About page</h1>
-    </>
+    <section className="text-center pb-10">
+      <div className="flex justify-center w-full">
+        <img
+          className="w-full"
+          src="https://images.unsplash.com/photo-1478720568477-152d9b164e26?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdmllfGVufDB8fDB8fHww"
+          alt="aboutpicture"
+        />
+      </div>
+      <h1 className="text-4xl font-bold mt-6">Welcome Everyone 🙌</h1>
+      <div className="mx-6">
+        <p className="my-6">
+          This website is to show the information of movie and videos that are
+          related to the movie you clicked
+        </p>
+        <p>Please, check movies you want to know !</p>
+      </div>
+    </section>
   );
 }
